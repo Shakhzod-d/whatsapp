07c569346233e2c796128e7df9f8e08e4feb861e
Shakhzod <forworkingappsinmylaptop@gmail.com>
2023-05-18 14:18:19 +0500

    html, css is done

---
+++ src/api.js
@@ -0,0 +1,2 @@
+/** PERSONAL ACCOUNT LATER I WILL CHANGE THIS TO ANY USER CAN JOIN */
+export const url = `https://api.green-api.com/waInstance1101821105/qr/2d6fedf9ad534d4ca851e62e32e457ea07b68b320a764fdb9b`;
